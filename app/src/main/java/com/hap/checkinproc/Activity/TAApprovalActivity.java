78a3f052323e338c8dbeef98c737f0ff95c01c83
Karthic kumar <ekumar.san@gmail.com>
2021-02-13 11:15:54 -0800

    2/13/2021 11.15 a.m

--- app/src/main/java/com/hap/checkinproc/Activity/TAApprovalActivity.java
+++ app/src/main/java/com/hap/checkinproc/Activity/TAApprovalActivity.java
@@ -1,152 +1,187 @@
 package com.hap.checkinproc.Activity;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.OnBackPressedDispatcher;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.hap.checkinproc.Activity.Util.ApprovalModel;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.hap.checkinproc.Activity_Hap.Approvals;
+import com.hap.checkinproc.Activity_Hap.Dashboard;
+import com.hap.checkinproc.Activity_Hap.Dashboard_Two;
+import com.hap.checkinproc.Activity_Hap.ERT;
+import com.hap.checkinproc.Activity_Hap.Help_Activity;
+import com.hap.checkinproc.Activity_Hap.Leave_Approval_Reject;
+import com.hap.checkinproc.Common_Class.Common_Class;
+import com.hap.checkinproc.Common_Class.Shared_Common_Pref;
+import com.hap.checkinproc.Interface.AdapterOnClick;
 import com.hap.checkinproc.Interface.ApiClient;
 import com.hap.checkinproc.Interface.ApiInterface;
+import com.hap.checkinproc.Model_Class.Leave_Approval_Model;
 import com.hap.checkinproc.R;
+import com.hap.checkinproc.adapters.Leave_Approval_Adapter;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static com.hap.checkinproc.Activity_Hap.Leave_Request.CheckInfo;
+
 public class TAApprovalActivity extends AppCompatActivity {
-    ListView list;
-    ApiInterface apiInterface;
-    ArrayList<ApprovalModel> array = new ArrayList<>();
-    SharedPreferences UserDetails;
-    public static final String MyPREFERENCES = "MyPrefs";
-    String SF_code = "", div = "";
+    List<Leave_Approval_Model> approvalList;
+    Gson gson;
+    private RecyclerView recyclerView;
+    Type userType;
+    Common_Class common_class;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_t_a_approval);
-        list = findViewById(R.id.list);
-        UserDetails = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-        SF_code = UserDetails.getString("Sfcode", "");
-        div = UserDetails.getString("Divcode", "");
-        apiInterface = ApiClient.getClient().create(ApiInterface.class);
-        callApi("2021-02-05");
+
+        common_class = new Common_Class(this);
+        TextView txtHelp = findViewById(R.id.toolbar_help);
+        ImageView imgHome = findViewById(R.id.toolbar_home);
+        txtHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Help_Activity.class));
+            }
+        });
+        TextView txtErt = findViewById(R.id.toolbar_ert);
+        TextView txtPlaySlip = findViewById(R.id.toolbar_play_slip);
+
+        txtErt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ERT.class));
+            }
+        });
+        txtPlaySlip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        ObjectAnimator textColorAnim;
+        textColorAnim = ObjectAnimator.ofInt(txtErt, "textColor", Color.WHITE, Color.TRANSPARENT);
+        textColorAnim.setDuration(500);
+        textColorAnim.setEvaluator(new ArgbEvaluator());
+        textColorAnim.setRepeatCount(ValueAnimator.INFINITE);
+        textColorAnim.setRepeatMode(ValueAnimator.REVERSE);
+        textColorAnim.start();
+        imgHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences CheckInDetails = getSharedPreferences(CheckInfo, Context.MODE_PRIVATE);
+                Boolean CheckIn = CheckInDetails.getBoolean("CheckIn", false);
+                if (CheckIn == true) {
+                    Intent Dashboard = new Intent(getApplicationContext(), Dashboard_Two.class);
+                    Dashboard.putExtra("Mode", "CIN");
+                    startActivity(Dashboard);
+                } else
+                    startActivity(new Intent(getApplicationContext(), Dashboard.class));
+
+
+            }
+        });
+        recyclerView = findViewById(R.id.leaverecyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        gson = new Gson();
+        getleavedetails();
+        ImageView backView = findViewById(R.id.imag_back);
+        backView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                common_class.CommonIntentwithFinish(Approvals.class);
+
+            }
+        });
     }
 
-    public void callApi(String date) {
-        try {
-            JSONObject jj = new JSONObject();
-            //jj.put("Ta_Date", "2020-11-13");
-            jj.put("div", div);
-            jj.put("sf", SF_code);  //MGR5246
-            jj.put("rSF", SF_code);
-            Log.v("json_obj_ta", jj.toString());
-            Call<ResponseBody> Callto = apiInterface.getTAAproval(jj.toString());
-            Callto.enqueue(new Callback<ResponseBody>() {
-                @Override
-                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                    try {
-                        if (response.isSuccessful()) {
-                            Log.v("print_upload_file_true", "ggg" + response);
-                            JSONObject jb = null;
-                            String jsonData = null;
-                            jsonData = response.body().string();
-                            Log.v("response_data", jsonData);
-                            JSONArray ja = new JSONArray(jsonData);
-                            for (int i = 0; i < ja.length(); i++) {
-                                JSONObject jj = ja.getJSONObject(i);
-                                array.add(new ApprovalModel(jj.getString("name"), jj.getString("Start_date"), jj.toString(), jj.getJSONArray("value").toString()));
-                            }
-                          /*  JSONObject json=new JSONObject(jsonData);
-                            JSONArray ja=json.getJSONArray("TAStartandEnd");
-                            JSONArray ja1=json.getJSONArray("User_Expense");
-                            String name,date;
-                            for(int i=0;i<ja.length();i++){
-                                JSONObject jjson=ja.getJSONObject(i);
-                                name=jjson.getString("FieldForceName");
-                                date=jjson.getString("Start_date");
-                                array.add(new ApprovalModel(name,date,jjson.toString(),ja1.toString()));
-                            }*/
+    public void getleavedetails() {
+        String routemaster = " {\"orderBy\":\"[\\\"name asc\\\"]\",\"desig\":\"mgr\"}";
+        ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
 
-                            AdapterForTAApproval adpt = new AdapterForTAApproval(TAApprovalActivity.this);
-                            list.setAdapter(adpt);
-                        }
-                    } catch (Exception e) {
+        Call<Object> mCall = apiInterface.GetTPObject(Shared_Common_Pref.Div_Code, Shared_Common_Pref.Sf_Code, Shared_Common_Pref.Sf_Code, Shared_Common_Pref.StateCode, "vwLeave", routemaster);
+
+        mCall.enqueue(new Callback<Object>() {
+            @Override
+            public void onResponse(Call<Object> call, Response<Object> response) {
+                // locationList=response.body();
+                Log.e("GetCurrentMonth_Values", String.valueOf(response.body().toString()));
+                Log.e("TAG_TP_RESPONSE", "response Tp_View: " + new Gson().toJson(response.body()));
+
+                userType = new TypeToken<ArrayList<Leave_Approval_Model>>() {
+                }.getType();
+                approvalList = gson.fromJson(new Gson().toJson(response.body()), userType);
+                Log.e("Leave_Adapter", String.valueOf(approvalList));
+
+
+                recyclerView.setAdapter(new Leave_Approval_Adapter(approvalList, R.layout.leave_approval_layout, getApplicationContext(), new AdapterOnClick() {
+                    @Override
+                    public void onIntentClick(int Name) {
+                        Intent intent = new Intent(TAApprovalActivity.this, Leave_Approval_Reject.class);
+                        intent.putExtra("LeaveId", String.valueOf(approvalList.get(Name).getLeaveId()));
+                        intent.putExtra("Username", approvalList.get(Name).getFieldForceName());
+                        intent.putExtra("Emp_Code", approvalList.get(Name).getEmpCode());
+                        intent.putExtra("HQ", approvalList.get(Name).getHQ());
+                        intent.putExtra("Designation", approvalList.get(Name).getDesignation());
+                        intent.putExtra("MobileNumber", approvalList.get(Name).getSFMobile());
+                        intent.putExtra("Reason", approvalList.get(Name).getReason());
+                        intent.putExtra("Leavetype", approvalList.get(Name).getLeaveName());
+                        intent.putExtra("fromdate", approvalList.get(Name).getFromDate());
+                        intent.putExtra("todate", approvalList.get(Name).getToDate());
+                        intent.putExtra("leavedays", String.valueOf(approvalList.get(Name).getLeaveDays()));
+                        Log.e("LEAVE_APPROVAL_REJECT", String.valueOf(approvalList.get(Name).getLeaveDays()));
+                        intent.putExtra("Sf_Code", approvalList.get(Name).getSfCode());
+                        startActivity(intent);
+                        Log.e("Leave_Type", approvalList.get(Name).getLeaveType().toString());
+
                     }
-                }
+                }));
+            }
 
+            @Override
+            public void onFailure(Call<Object> call, Throwable t) {
+
+            }
+        });
+
+    }
+
+    private final OnBackPressedDispatcher mOnBackPressedDispatcher =
+            new OnBackPressedDispatcher(new Runnable() {
                 @Override
-                public void onFailure(Call<ResponseBody> call, Throwable t) {
-
+                public void run() {
+                    TAApprovalActivity.super.onBackPressed();
                 }
             });
 
-        } catch (Exception e) {
-        }
+    @Override
+    public void onBackPressed() {
+
     }
 
-    public class AdapterForTAApproval extends BaseAdapter {
-        Context context;
 
-        public AdapterForTAApproval(Context context) {
-            this.context = context;
-        }
-
-        @Override
-        public int getCount() {
-            return array.size();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return array.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            view = LayoutInflater.from(context).inflate(R.layout.row_item_approval, viewGroup, false);
-            TextView txt_name = view.findViewById(R.id.txt_name);
-            TextView txt_dt = view.findViewById(R.id.txt_dt);
-            TextView txt_open = view.findViewById(R.id.txt_open);
-            txt_name.setText(array.get(i).getName());
-            txt_dt.setText(array.get(i).getDates());
-            txt_open.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    try {
-
-                        Intent ii = new Intent(TAApprovalActivity.this, ApprovalViewActivity.class);
-                        ii.putExtra("head_val", array.get(i).getFirstdata());
-                        startActivity(ii);
-
-
-                    } catch (Exception e) {
-                    }
-                }
-            });
-
-            return view;
-        }
-    }
 }
