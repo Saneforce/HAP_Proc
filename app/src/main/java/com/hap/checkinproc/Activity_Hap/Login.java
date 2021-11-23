c4af8ee26733a7d7aa48f8ad8e1ddce6b4545f5a
Anbu <anbu@saneforce.com>
2021-11-23 11:29:57 +0530

    HAPSFA 3.2.0 23NOV2021

--- app/src/main/java/com/hap/checkinproc/Activity_Hap/Login.java
+++ app/src/main/java/com/hap/checkinproc/Activity_Hap/Login.java
@@ -573,9 +573,9 @@ public class Login extends AppCompatActivity {
         }
 
         //eMail = "nallathambi.a@hap.in";
-        eMail = "1005985@hap.in";
+        //eMail = "1005985@hap.in";
         //eMail = "haptest3@hap.in";
-         //eMail = "ciadmin@hap.in";
+        //eMail = "ciadmin@hap.in";
 
         Call<Model> modelCall = apiInterface.login("get/GoogleLogin", eMail,BuildConfig.VERSION_NAME, deviceToken);
         modelCall.enqueue(new Callback<Model>() {
