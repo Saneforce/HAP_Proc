c948246ae8650b98c9d21bcbad4f57452d6e6254
anbuj <anbu@saneforce.com>
2022-10-18 14:25:52 +0530

    Secondary Sales and invoice and vansales

--- app/src/main/java/com/hap/checkinproc/SFA_Activity/VanStockViewActivity.java
+++ app/src/main/java/com/hap/checkinproc/SFA_Activity/VanStockViewActivity.java
@@ -20,6 +20,7 @@ import com.hap.checkinproc.Common_Class.Shared_Common_Pref;
 import com.hap.checkinproc.Interface.UpdateResponseUI;
 import com.hap.checkinproc.Model_Class.Help_Model;
 import com.hap.checkinproc.R;
+import com.hap.checkinproc.SFA_Adapter.rvVanStockview;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -33,9 +34,8 @@ public class VanStockViewActivity extends AppCompatActivity implements View.OnCl
     Common_Class common_class;
     TextView tvDt, tvLoadAmt, tvUnLoadAmt, tvTotVanSalQty, tvTotStkQty;
     NumberFormat formatter = new DecimalFormat("##0.00");
-    RecyclerView rvVanSales;
-    private double salAmt;
-    private int totStk, totSal;
+    RecyclerView rvVanStockDets;
+
     private DatePickerDialog fromDatePickerDialog;
 
     public static TextView tvStartDate, tvEndDate;
@@ -53,13 +53,10 @@ public class VanStockViewActivity extends AppCompatActivity implements View.OnCl
         sharedCommonPref = new Shared_Common_Pref(VanStockViewActivity.this);
         common_class = new Common_Class(this);
 
+        rvVanStockDets=findViewById(R.id.idStockDets);
 
         tvLoadAmt = findViewById(R.id.tvLoadStkAmt);
         tvUnLoadAmt = findViewById(R.id.tvUnLoadStkAmt);
-        tvDt = findViewById(R.id.tvVSPayDate);
-        rvVanSales = findViewById(R.id.rvVanSal);
-        tvTotVanSalQty = findViewById(R.id.tvTotSalQty);
-        tvTotStkQty = findViewById(R.id.tvTotLoadQty);
 
         tvStartDate = findViewById(R.id.tvStartDate);
         tvEndDate = findViewById(R.id.tvEndDate);
@@ -77,8 +74,6 @@ public class VanStockViewActivity extends AppCompatActivity implements View.OnCl
 //        tvStartDate.setText(Common_Class.GetDatewothouttime());
 //        tvEndDate.setText(Common_Class.GetDatewothouttime());
 
-        tvDt.setText("Date : " + Common_Class.GetDatemonthyearformat());
-        tvLoadAmt.setText("₹" + formatter.format(getIntent().getDoubleExtra("stkLoadAmt", 0)));
         common_class.getDb_310Data(Constants.VAN_STOCK, this);
 
         ImageView ivToolbarHome = findViewById(R.id.toolbar_home);
@@ -162,24 +157,25 @@ public class VanStockViewActivity extends AppCompatActivity implements View.OnCl
         try {
 
             JSONObject stkObj = new JSONObject(apiDataResponse);
-            salAmt = 0;
-            totStk = 0;
-            totSal = 0;
+//            salAmt = 0;
+//            totStk = 0;
+//            totSal = 0;
             JSONArray arr = stkObj.getJSONArray("Data");
 
-            for (int i = 0; i < arr.length(); i++) {
-                JSONObject obj = arr.getJSONObject(i);
-
-                totStk += obj.getInt("Cr");
-                totSal += obj.getInt("Dr");
-
-            }
-            tvTotVanSalQty.setText("" + totSal);
-            tvTotStkQty.setText("" + totStk);
+//            for (int i = 0; i < arr.length(); i++) {
+//                JSONObject obj = arr.getJSONObject(i);
+//
+//                totStk += obj.getInt("Cr");
+//                totSal += obj.getInt("Dr");
+//
+//            }
+//            tvTotVanSalQty.setText("" + totSal);
+//            tvTotStkQty.setText("" + totStk);
 
 
           //  tvUnLoadAmt.setText("₹" + formatter.format(getIntent().getDoubleExtra("stkLoadAmt", 0) - salAmt));
-            rvVanSales.setAdapter(new Pay_Adapter(arr, R.layout.adapter_vansales_stockview, VanStockViewActivity.this));
+
+            rvVanStockDets.setAdapter(new rvVanStockview(arr, R.layout.layvanstockadb, this));
 
         } catch (Exception e) {
             Log.v("adap:", e.getMessage());
@@ -187,80 +183,6 @@ public class VanStockViewActivity extends AppCompatActivity implements View.OnCl
 
     }
 
-    public class Pay_Adapter extends RecyclerView.Adapter<Pay_Adapter.MyViewHolder> {
-        Context context;
-        private JSONArray arr;
-        private int rowLayout;
 
-
-        public Pay_Adapter(JSONArray arr, int rowLayout, Context context) {
-            this.arr = arr;
-            this.rowLayout = rowLayout;
-            this.context = context;
-
-
-        }
-
-        @Override
-        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(rowLayout, parent, false);
-            return new MyViewHolder(view);
-        }
-
-        @Override
-        public int getItemViewType(int position) {
-            return position;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public void onBindViewHolder(MyViewHolder holder, int position) {
-            try {
-
-
-                JSONObject obj = arr.getJSONObject(position);
-
-                try {
-                    holder.tvSku.setText("" + (obj.getString("PName")));
-                } catch (Exception e) {
-
-                }
-
-                holder.tvSalQty.setText(String.valueOf("" + obj.getInt("Dr")).toString().trim());
-
-                holder.tvStkQty.setText("" + (obj.getInt("Cr")));
-
-
-            } catch (Exception e) {
-                Log.e("adapterProduct: ", e.getMessage());
-            }
-
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return arr.length();
-        }
-
-        public class MyViewHolder extends RecyclerView.ViewHolder {
-            public TextView tvSku, tvStkQty, tvSalQty;
-
-
-            public MyViewHolder(View view) {
-                super(view);
-                tvSku = view.findViewById(R.id.tvSku);
-                tvStkQty = view.findViewById(R.id.tvStockQty);
-                tvSalQty = view.findViewById(R.id.tvSalQty);
-
-            }
-        }
-
-
-    }
 
 }
